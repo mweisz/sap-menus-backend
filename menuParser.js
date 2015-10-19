afd94808bf662ef9c37b88b39dac568b43666cc0
Jonas Keutel <jonas@keutel.de>
2015-10-18 21:15:08 -0700

    menuParser now returns json dictionary with all cafes and their corresponding meals (only the label)

--- menuParser.js
+++ menuParser.js
@@ -13,18 +13,44 @@ exports.parseMenu = function (req, res) {
     };
 
     request(callOptions, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var rawMenu = JSON.parse(body);
-            var categories = rawMenu.days[0].cafes['245'].dayparts[0][1].stations;
-
-            for (var i = 0; i < categories.length; i++ ) {
-                console.log(categories[i].label);
-            }
-
-            res.send(rawMenu.days[0].cafes['245'].name);
-
-        } else {
+        if (error || response.statusCode != 200) {
             console.log("Error while trying to make API call. Status Code: " + response.statusCode);
+            res.send("Error while trying to make API call. Status Code: " + response.statusCode)
+            return(response.statusCode)
         }
+
+        var resultJson = {};
+
+        var rawMenu = JSON.parse(body);
+        
+        // Café 3
+        console.log('enter first call allItemsForCafe')
+        resultJson.cafe1 = allItemsForCafe(rawMenu, '246')
+        
+        // Café 3
+        resultJson.cafe3 = allItemsForCafe(rawMenu, '245')
+
+        // Café 3
+        resultJson.cafe8 = allItemsForCafe(rawMenu, '247')
+
+        res.send(resultJson);
+        return;
     })
+}
+
+// TODO: Error handling
+function allItemsForCafe (rawMenu, id) {
+    var categories = rawMenu.days[0].cafes[id].dayparts[0][1].stations;
+    var items = {};
+
+    var i = 0;
+    for (var j = 0; j < categories.length - 1; j++) {
+        for (var k = 0; k < categories[j].items.length; k++) {
+            itemId = categories[j].items[k];
+            items[i] = rawMenu.items[itemId].label;
+            i++;
+        }
+    };
+
+    return items;
 }
